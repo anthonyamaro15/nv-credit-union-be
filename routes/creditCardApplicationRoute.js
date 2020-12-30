8192d2f31aef795495fafc420ba9f3fa7fd1bcff
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-12-30 14:03:53 -0800

    refactor(credit-card-application
    rename email function

--- routes/creditCardApplicationRoute.js
+++ routes/creditCardApplicationRoute.js
@@ -9,65 +9,15 @@ const route = express.Router();
 route.post("/create", async (req, res) => {
    const { applicationNumber, firstName,  contactEmail } = req.body;
    try {
-      // callMethod({applicationNumber, firstName,  contactEmail})
-      test({applicationNumber, firstName,  contactEmail});
+      
+      sendEmail({applicationNumber, firstName,  contactEmail});
       res.status(201).json({ message: "email sent!" });      
    } catch (error) {
       res.status(500).json({ errorMessage: error.message });
    }
 });
 
-const obj = {
-  subject: "Your Loan Application Status",
-  heading: "Welcome to my Page",
-  description: "here is an example of the data im gonna be sending to you",
-};
-
-
-const callMethod = (body) => {
-   axios({
-      method: 'POST',
-      url: 'https://api.sendgrid.com/v3/mail/send',
-      headers: {
-         'content-type': 'application/json',
-         Authorization: sendGridKey
-      },
-      data: {
-         personalizations: [
-            {
-               to: [
-                  {
-                     email: body.contactEmail,
-                     name: body.firstName
-                  },
-               ],
-               subject: obj.subject
-            },
-         ],
-         from: {
-            email: sender,
-            name: "loans@onenevada.org"
-         },
-         content: [
-            {
-               type: 'text/html',
-               value: applicationTemplate(body)
-            }
-         ]
-      }
-   })
-   .then((res) => {
-      console.log(res.status);
-   })
-   .catch((error) => {
-      console.log( error.response);
-   })
-};
-
-module.exports = route;
-
-
-function test(body) {
+function sendEmail(body) {
    sgMail.setApiKey(sendGridKey)
    const msg = {
       to: body.contactEmail,
@@ -86,3 +36,4 @@ function test(body) {
    })
 }
 
+module.exports = route;
